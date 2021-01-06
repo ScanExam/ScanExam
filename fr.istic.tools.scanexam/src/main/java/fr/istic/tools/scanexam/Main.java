217c4034009249aa3ab3ea49ba33730019b2ee0c
Théo <theo.giraudet@orange.fr>
2021-01-06 15:22:10 +0100

    Add Main

---
+++ fr.istic.tools.scanexam/src/main/java/fr/istic/tools/scanexam/Main.java
@@ -0,0 +1,10 @@
+package fr.istic.tools.scanexam;
+
+import org.eclipse.xtext.xbase.lib.InputOutput;
+
+@SuppressWarnings("all")
+public class Main {
+  public static void main(final String[] args) {
+    InputOutput.<String>println("Hello World!");
+  }
+}
---
+++ fr.istic.tools.scanexam/src/main/xtend/fr/istic/tools/scanexam/Main.xtend
@@ -0,0 +1,9 @@
+package fr.istic.tools.scanexam
+
+class Main {
+	
+	def static void main(String[] args) {
+		println("Hello World!")
+	}
+	
+}
