afc112d71a4f759db021a9c61cb42a33be40a383
Ghouti Terki Rida <rida.ght@gmail.com>
2021-03-17 15:22:07 +0100

    [SWING] Ajout de commentaires

--- fr.istic.tools.scanexam/src/main/xtend-gen/fr/istic/tools/scanexam/api/Observer.java
+++
@@ -1,8 +0,0 @@
-package fr.istic.tools.scanexam.api;
-
-import fr.istic.tools.scanexam.api.Subject;
-
-@SuppressWarnings("all")
-public interface Observer<T extends Object> {
-  void update(final Subject<T> subject, final T data);
-}
