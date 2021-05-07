46792cff0d83cb5f965cc8c93cc18425af581712
Danlos Benjamin <benjamin.danlos@etudiant.univ-rennes1.fr>
2021-05-07 09:37:22 +0200

    More tests

--- fr.istic.tools.scanexam/src/test/java/fr/istic/tools/scanexam/interfaceFX/PdfManagerTest.java
+++ fr.istic.tools.scanexam/src/test/java/fr/istic/tools/scanexam/interfaceFX/PdfManagerTest.java
@@ -1,7 +1,12 @@
 package fr.istic.tools.scanexam.interfaceFX;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -86,4 +91,43 @@ public class PdfManagerTest {
 		assertEquals(0, manager.getPdfPageIndex());
 	}
 	
+	@Test
+	@DisplayName("Test number of pages")
+	void testNumberOfPages() {
+		assertEquals(7, manager.getPdfPageCount());
+	}
+
+	@Test
+	@DisplayName("Test correct page")
+	void testCorrectPage() {
+		assertTrue(manager.atCorrectPage(0));
+	}
+
+	@Test
+	@DisplayName("Test correct page after change")
+	void testCorrectPageAfterChange() {
+		manager.nextPdfPage();
+		assertTrue(manager.atCorrectPage(1));
+	}
+
+	@Test
+	@DisplayName("Test output stream not null")
+	void testPdfOutputStreamNotNull() {
+		ByteArrayOutputStream stream = manager.getPdfOutputStream();
+		assertNotNull(stream);
+	}
+
+	@Test
+	@DisplayName("Test input stream not null")
+	void testPdfInputStreamNotNull() {
+		ByteArrayInputStream stream = manager.getPdfInputStream();
+		assertNotNull(stream);
+	}
+	
+	@Test
+	@DisplayName("Test pdf to image not null")
+	void testPdfToImageNotNull() {
+		BufferedImage image = manager.getCurrentPdfPage();
+		assertNotNull(image);
+	}
 }
